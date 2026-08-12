bfa5af65bb111253f1e03531f9573a76520f51f9
Kamakxz <igor.almeidado.nascimento@escola.pr.gov.br>
2026-08-12 20:01:24 -0300

    config: estrutura inicial de pastas, sistema inicializado.

---
+++ src/test/java/br/uniamerica/taskq/TaskQApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uniamerica.taskq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskQApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

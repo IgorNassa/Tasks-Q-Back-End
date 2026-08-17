7361fa71b1dd4e384d4d468533a60fa00e88d74e
felipePiquetti <piquetti6@gmail.com>
2026-08-16 22:52:15 -0300

    feat: implementa estrutura inicial do service de usuarios

---
+++ src/main/java/br/uniamerica/taskq/service/UserService.java
@@ -0,0 +1,45 @@
+package br.uniamerica.taskq.service;
+
+import br.uniamerica.taskq.controller.dto.response.UserResponse;
+import br.uniamerica.taskq.controller.dto.resquest.UserRequest;
+import br.uniamerica.taskq.entity.User;
+import br.uniamerica.taskq.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User create(UserRequest userRequest){
+        return userRepository.save(userRequest.toUser());
+    }
+
+    public List<UserResponse> findAll(){
+
+        return userRepository.findAll()
+                .stream()
+                .map(UserResponse::from)
+                .toList();
+    }
+
+    public UserResponse update(Long id, UserRequest atualizado){
+        userRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+
+         var user = atualizado.toUser();
+         user.setId(id);
+
+         var userAtualizado = userRepository.save(user);
+
+        return UserResponse.from(userAtualizado);
+    }
+
+    public void delete (UserRequest userRequest){
+        userRepository.deleteById(userRequest.toUser().getId());
+    }
+}
